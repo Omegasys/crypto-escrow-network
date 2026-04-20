ffedab122764d7c0235036bb83ae436a998ece74
Omega <emomega@proton.me>
2026-04-20 00:45:58 -0400

    Create mixer_2.py

---
+++ src/mixers/mixer_2.py
@@ -0,0 +1,18 @@
+import random
+import time
+from wallet_2A_6F import MixerWallet
+from mixer_utils import apply_delay
+
+class Mixer2:
+    def __init__(self, io_ports=32, delay_range=(1, 9)):
+        self.io_ports = io_ports
+        self.delay_range = delay_range
+        self.wallets = [MixerWallet(balance=0.0, address=f"Mixer2 Wallet {i}") for i in range(1, io_ports+1)]
+
+    def mix(self, amount, sender_wallet):
+        """Mixes funds by transferring to one of the IO ports and applying delay."""
+        target_wallet = random.choice(self.wallets)
+        apply_delay(self.delay_range)
+        sender_wallet.transfer(target_wallet, amount)
+        print(f"Transferred {amount} from {sender_wallet.address} to {target_wallet.address} in Mixer 2")
+        return target_wallet
